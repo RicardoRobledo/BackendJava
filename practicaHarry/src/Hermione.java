8966659f7e3fbd4041d0f4f19f6fd6062fd69653
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-17 11:50:25 -0500

    feat(practicaHarry): Hermione class was updated
    Hermione's class's constructor was added

--- practicaHarry/src/Hermione.java
+++ practicaHarry/src/Hermione.java
@@ -8,4 +8,12 @@ public class Hermione {
     private String boggart;
     private String patronus;
 
+    public Hermione() {
+        this.casa = "Gryffindor";
+        this.nombre = "Hermione";
+        this.genero = 'F';
+        this.boggart = "Mcgonagall diciendole que reprobo todo";
+        this.patronus = "Nutria";
+    }
+
 }
