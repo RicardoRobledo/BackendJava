364ab55556f67158c7ab809fbc84a8cfd3e81cf7
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-17 18:38:53 -0500

    feat(practicaMona): Prueba class was modified
    main method was added and the objects for all the classes

--- practicaMona/src/Prueba.java
+++ practicaMona/src/Prueba.java
@@ -1,8 +1,24 @@
+/**
+ * This class is for do the tests
+ *
+ * @author: Ricardo
+ * */
+
 public class Prueba {
 
     public static void main(String[] args) {
 
-        
+        Mona ma = new MonaAstronauta();
+        Mona mb = new MonaBajista();
+        Mona mj = new MonaJetpack();
+        Mona ml = new MonaLuchadora();
+        Mona mp = new MonaProgramadora();
+
+        ma.mainActivity();
+        mb.mainActivity();
+        mj.mainActivity();
+        ml.mainActivity();
+        mp.mainActivity();
 
     }
 
