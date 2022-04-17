3c4302a451d8ae33af22b307197459735d9870f5
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-17 11:51:23 -0500

    feat(practicaHarry): Hermione class was updated
    setters and getters were added

--- practicaHarry/src/Hermione.java
+++ practicaHarry/src/Hermione.java
@@ -16,4 +16,44 @@ public class Hermione {
         this.patronus = "Nutria";
     }
 
+    public String getCasa() {
+        return casa;
+    }
+
+    public void setCasa(String casa) {
+        this.casa = casa;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public char getGenero() {
+        return genero;
+    }
+
+    public void setGenero(char genero) {
+        this.genero = genero;
+    }
+
+    public String getBoggart() {
+        return boggart;
+    }
+
+    public void setBoggart(String boggart) {
+        this.boggart = boggart;
+    }
+
+    public String getPatronus() {
+        return patronus;
+    }
+
+    public void setPatronus(String patronus) {
+        this.patronus = patronus;
+    }
+
 }
