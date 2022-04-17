9c81d1198f2c2885582c345a4766870f2e28cacf
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-17 11:58:39 -0500

    feat(practicaHarry): 5 variables were added in Neville class
    'casa', 'nombre', 'genero', 'boggart' and 'pratonus' have been added

--- practicaHarry/src/Neville.java
+++ practicaHarry/src/Neville.java
@@ -1,4 +1,11 @@
 package src;
 
 public class Neville {
+
+    private String casa;
+    private String nombre;
+    private char genero;
+    private String boggart;
+    private String patronus;
+
 }
