db16981a769fc6f8fca1df8ec633d5f2c264b6b1
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-17 13:52:59 -0500

    feat(practicaMona): MonaBajista class was modified
    mainActivity method was overrided

--- practicaMona/src/MonaBajista.java
+++ practicaMona/src/MonaBajista.java
@@ -5,4 +5,13 @@
  * */
 
 public class MonaBajista extends Mona{
+
+    /**
+     * This method define what MonaBajista likes to do
+     * */
+    @Override
+    public void mainActivity() {
+        System.out.println("Mona bajista likes to play the bass");
+    }
+
 }
