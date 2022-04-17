2ba873c53089e11bcf9ea50bf934a226905ea6fa
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-17 12:13:36 -0500

    feat(practicaHarry): Prueba class was modified
    Objects of 'Draco', 'Harry', 'Hermione', 'Neville' and 'Ron' were created

--- practicaHarry/src/Prueba.java
+++ practicaHarry/src/Prueba.java
@@ -4,6 +4,18 @@ public class Prueba {
 
     public static void main(String[] args) {
 
+        Draco draco = new Draco();
+        Harry harry = new Harry();
+        Hermione hermione = new Hermione();
+        Neville neville = new Neville();
+        Ron ron = new Ron();
+
+        System.out.println(harry);
+        System.out.println(hermione);
+        System.out.println(neville);
+        System.out.println(ron);
+        System.out.println(draco);
+
     }
 
 }
