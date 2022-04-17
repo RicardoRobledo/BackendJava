bad9d514a49a61f47b09270475d58894a7773455
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-17 13:39:08 -0500

    feat(practicaMona): Mona class has been modified
    mainActivity method was added

--- practicaMona/src/Mona.java
+++ practicaMona/src/Mona.java
@@ -1,2 +1,15 @@
+/**
+ * This class define the entity Mona
+ *
+ * @author: Ricardo
+ * */
+
+
 public class Mona {
+
+    /**
+     * This method define the main action of a Mona entity
+     * */
+    public void mainActivity(){}
+
 }
