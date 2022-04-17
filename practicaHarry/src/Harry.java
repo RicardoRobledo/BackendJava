088212451094eff1c78f016ca1ebb8f4108b0603
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-17 11:45:44 -0500

    feat(practicaHarry): Harry class was updated
    setters and getters were added

--- practicaHarry/src/Harry.java
+++ practicaHarry/src/Harry.java
@@ -16,5 +16,45 @@ public class Harry {
         this.patronus = "ciervo";
     }
 
+    public String getCasa() {
+        return casa;
+    }
+
+    public void setCasa(String casa) {
+        this.casa = casa;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public char getGenero() {
+        return genero;
+    }
+
+    public void setGenero(char genero) {
+        this.genero = genero;
+    }
+
+    public String getBoggart() {
+        return boggart;
+    }
+
+    public void setBoggart(String boggart) {
+        this.boggart = boggart;
+    }
+
+    public String getPatronus() {
+        return patronus;
+    }
+
+    public void setPatronus(String patronus) {
+        this.patronus = patronus;
+    }
+
     
 }
