252baea95c8a1b30585967a925fcdcad32085135
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-17 13:49:21 -0500

    feat(practicaMona): MonaAstronauta class was modified
    mainActivity method was overrided

--- practicaMona/src/MonaAstronauta.java
+++ practicaMona/src/MonaAstronauta.java
@@ -1,2 +1,17 @@
+/**
+ * This class define the entity MonaAtronauta
+ *
+ * @author: Ricardo
+ * */
+
 public class MonaAstronauta extends Mona{
+
+    /**
+     * This method define what MonaAstronauta likes to do
+     * */
+    @Override
+    public void mainActivity() {
+        System.out.println("Mona astronauta likes to travel to the moon");
+    }
+
 }
