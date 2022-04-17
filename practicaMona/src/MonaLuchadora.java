ea3ea94e4a3c59177d1431690796beddc2da1e00
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-17 13:44:51 -0500

    feat(practicaMona): MonaLuchadora class was modified
    mainActivity method was overrided

--- practicaMona/src/MonaLuchadora.java
+++ practicaMona/src/MonaLuchadora.java
@@ -1,2 +1,16 @@
+/**
+ * This class define the entity MonaLuchadora
+ *
+ * @author: Ricardo
+ * */
+
 public class MonaLuchadora extends Mona{
+
+    /**
+     * This method define what MonaLuchadora likes to do
+     * */
+    @Override
+    public void mainActivity() {
+        System.out.println("Mona luchadora likes to fight");
+    }
 }
