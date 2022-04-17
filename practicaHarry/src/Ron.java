1b1dabeede0e71fa496c791bcf9a1a1087f82408
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-17 12:06:11 -0500

    feat(practicaHarry): Ron class was updated
    Ron's class's constructor was added

--- practicaHarry/src/Ron.java
+++ practicaHarry/src/Ron.java
@@ -8,4 +8,12 @@ public class Ron {
     private String boggart;
     private String patronus;
 
+    public Ron() {
+        this.casa = "Gryffindor";
+        this.nombre = "Ron";
+        this.genero = 'M';
+        this.boggart = "Acromántula";
+        this.patronus = "Jack Russell Terrier";
+    }
+
 }
