d0c31c72aaeb11ccd115cb0f37eef33b1f6993bb
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-17 12:01:17 -0500

    feat(practicaHarry): Neville class was updated
    Neville's class's constructor was added

--- practicaHarry/src/Neville.java
+++ practicaHarry/src/Neville.java
@@ -8,4 +8,12 @@ public class Neville {
     private String boggart;
     private String patronus;
 
+    public Neville() {
+        this.casa = "Gryffindor";
+        this.nombre = "Neville";
+        this.genero = 'M';
+        this.boggart = "Severus Snape";
+        this.patronus = "Non-Corporeal Patronus";
+    }
+
 }
