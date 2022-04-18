c7b6b155038581dcb1aa12816c106592818627c1
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-18 13:56:10 -0500

    fix(practicaComic): Marco class was updated
    Marco's constructor's class was added and 'text' variable was initialized

--- practicaComic/src/Marco.java
+++ practicaComic/src/Marco.java
@@ -1,2 +1,20 @@
+/**
+ * This class define the frames of the comic
+ *
+ * @author: Ricardo
+ */
+
 public class Marco {
+
+    private String text;
+
+    /**
+     * This constructor receive the text of the frame of the comic
+     *
+     * @param text
+     */
+    public Marco(String text){
+        this.text = text;
+    }
+
 }
--- practicaComic/src/Prueba.java
+++ practicaComic/src/Prueba.java
@@ -8,6 +8,21 @@ public class Prueba {
 
     public static void main(String[] args) {
 
+        Screen screen = new Screen();
+        Dialog dialog = new Dialog();
+        String str;
+
+        screen.out("Bienvenidos a la mision Backend Java");
+        screen.setVisible(true);
+
+        str = dialog.readString("¿Quieres aprender a hacer esto?");
+
+        if(str.charAt(0)=='S'||str.charAt(0)=='s'){
+            screen.out("Hola");
+        }else{
+
+        }
+
     }
 
 }
