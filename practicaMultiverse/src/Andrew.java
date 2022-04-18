b58cf7d9595d2d10d1f7a32114b32a27ff4327e7
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-17 20:34:37 -0500

    feat(practicaMultiverse): Andrew class was updated
    Now Andrew class was extended from Spiderman and the constructor was added

--- practicaMultiverse/src/Andrew.java
+++ practicaMultiverse/src/Andrew.java
@@ -1,2 +1,19 @@
-public class Andrew {
+/**
+ * This class define the base entity Andrew
+ *
+ * @author: Ricardo
+ * */
+
+
+public class Andrew extends Spiderman{
+
+    /**
+     * Constructor for the genre of the character
+     *
+     * @param genero
+     * @param: genero Tell us the genre of the character
+     */
+    public Andrew(char genero) {
+        super(genero);
+    }
 }
