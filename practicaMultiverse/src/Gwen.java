dbb25be8ea7d9f24f13dcbc11e0eddab86a18cba
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-17 20:30:40 -0500

    feat(practicaMultiverse): Gwen class was updated
    Gwen class extends from Spiderman and the constructor was added

--- practicaMultiverse/src/Gwen.java
+++ practicaMultiverse/src/Gwen.java
@@ -1,2 +1,18 @@
-public class Gwen {
+/**
+ * This class define the base entity Gwen
+ *
+ * @author: Ricardo
+ * */
+
+public class Gwen extends Spiderman{
+
+    /**
+     * Constructor for the genre of the character
+     *
+     * @param genero
+     * @param: genero Tell us the genre of the character
+     */
+    public Gwen(char genero) {
+        super(genero);
+    }
 }
