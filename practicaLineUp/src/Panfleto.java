b955131febe86fafd0e4b45da390aec4a64db7dd
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-19 14:35:28 -0500

    feat(practicaLineUp): 'Panfleto' class was modified
    setters and getters were added

--- practicaLineUp/src/Panfleto.java
+++ practicaLineUp/src/Panfleto.java
@@ -12,4 +12,11 @@ public class Panfleto{
         this.contenido=contenido;
     }
 
+    public String[] getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String[] contenido) {
+        this.contenido = contenido;
+    }
 }
