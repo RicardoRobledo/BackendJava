272f3dc21cad55345721e0820ea557681ca1b603
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-18 23:13:51 -0500

    feat(practicaLineUp): Persona class was modified
    'nombre' variable was added

--- practicaLineUp/src/Persona.java
+++ practicaLineUp/src/Persona.java
@@ -6,11 +6,14 @@
 
 public class Persona {
 
+    private String nombre;
+
     /**
      * This constructor give us the name of the person
      *
      * @param nombre
      */
     public Persona(String nombre){
+        this.nombre = nombre;
     }
 }
