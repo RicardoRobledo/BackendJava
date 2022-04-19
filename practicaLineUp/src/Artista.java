6b8805de377db8d5de89dac4f3e206358b8d60ea
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-18 23:00:51 -0500

    feat(practicaLineUp): 'Artista' class was modified
    'nombre' and 'role' variables were added and initialized

--- practicaLineUp/src/Artista.java
+++ practicaLineUp/src/Artista.java
@@ -6,8 +6,18 @@
 
 public class Artista {
 
-    public Artista(){
+    private String nombre;
+    private String rol;
 
+    /**
+     * This constructor give the name and the role of the artist
+     *
+     * @param nombre Define the name of the artist
+     * @param rol Define the role of the artist
+     */
+    public Artista(String nombre, String rol){
+        this.nombre = nombre;
+        this.rol = rol;
     }
 
 }
