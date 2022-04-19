a65ca9824a2b7cab594fb6131186d91be121984d
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-04-19 13:08:30 -0500

    feat(practicaLineUp): 'Layout' class was updated
    'Formateador' interface was implemented and 'darFormato' method was overrided

--- practicaLineUp/src/Layout.java
+++ practicaLineUp/src/Layout.java
@@ -1,11 +1,16 @@
+import java.util.ArrayList;
+
 /**
  * This class describe the Layout of each part of the announcement
  *
  * @author: Ricardo
  * */
 
-public class Layout {
+public class Layout implements Formateador{
 
 
-
+    @Override
+    public String darFormato(ArrayList<Banda> bandas) {
+        return null;
+    }
 }
